f0daf9a25fe23750e10d6a532476d08ec1e9d783
Makayla <makaylabusher@gmail.com>
2023-01-16 13:57:30 -0600

    add team model

---
+++ db/migrate/20230116195700_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :location
+      t.string :league
+      t.string :logo
+
+      t.timestamps
+    end
+  end
+end
